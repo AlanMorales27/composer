9aab86fa417b9fc07269a9500be274d057d43680
AlanMorales27 <alanmoralesg47@gmail.com>
2026-06-14 00:17:28 -0500

    feat: create canvas and table svg functions in react

---
+++ client/src/components/svg_components/RestaurantCanvas.tsx
@@ -0,0 +1,11 @@
+/**
+ * This component represents the canvas where the restaurant layout is displayed
+ */
+
+export default function RestaurantCanvas(){
+    return (
+        <svg>
+            
+        </svg>
+    )
+}
