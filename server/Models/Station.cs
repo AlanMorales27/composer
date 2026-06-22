e3ff4ca0bf4d9d3bde53c5bd2a73b3d6e2f756f3
AlanMorales27 <alanmoralesg47@gmail.com>
2026-06-21 23:39:55 -0500

    feat: add validation attributes for Station properties

--- server/Models/Station.cs
+++ server/Models/Station.cs
@@ -3,11 +3,20 @@ using System.ComponentModel.DataAnnotations;
 public class Station
 {
     public int Id { get; set; }
+    
+    [Required]
+    [Range(1, int.MaxValue)]
     public int Number { get; set; }
+
     [Required]
+    [Range(0, int.MaxValue)]
     public int X {get; set;}
+
     [Required]
+    [Range(0, int.MaxValue)]
     public int Y {get; set;}
+
     [Required]
+    [Range(2, int.MaxValue)]
     public int Capacity {get; set;}
 }
