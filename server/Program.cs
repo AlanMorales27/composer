38c1a241e645ab76fb5e23564df3f9f84b3bccd6
AlanMorales27 <alanmoralesg47@gmail.com>
2026-06-21 23:17:58 -0500

    feat: disengage connection string in program.cs

--- server/Program.cs
+++ server/Program.cs
@@ -1,9 +1,16 @@
+using Microsoft.EntityFrameworkCore;
+
 var builder = WebApplication.CreateBuilder(args);
 
 builder.Services.AddOpenApi();
 builder.Services.AddControllers();
-builder.Services.AddDbContext<AppDbContext>();
 builder.Services.AddScoped<StationService>();
+
+builder.Services.AddDbContext<AppDbContext>(
+    options => options
+        .UseSqlServer( builder.Configuration.GetConnectionString("DefaultConnection"))
+        .LogTo(Console.WriteLine, LogLevel.Information)
+);
 
 var app = builder.Build();
 
